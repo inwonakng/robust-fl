de6ce258377658ec522b28dcc4e03bc2a4eed377
inwonakng <inwon.kang04@gmail.com>
2023-04-09 04:04:53 -0400

    clusteragg verified

--- scripts/run_mnist.sh
+++ scripts/run_mnist.sh
@@ -1,4 +1,4 @@
-# bash scripts/fedavg.sh configurations/datasets/mnist_0_1.yaml
+bash scripts/fedavg.sh configurations/datasets/mnist_0_1.yaml
 bash scripts/rfa_whole.sh configurations/datasets/mnist_0_1.yaml
 bash scripts/rfa_component.sh configurations/datasets/mnist_0_1.yaml
 bash scripts/clusteragg_average.sh configurations/datasets/mnist_0_1.yaml
