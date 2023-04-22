fixed staleness weighting

src/aggregators/aggregators.py
@@ -9,9 +9,9 @@ from .utils import normalize_weights
 class Aggregator:
     def __init__(
         self, 
-        use_staleness: bool = False,
+        staleness_lambda: bool = 0,
     ) -> None:
-        self.use_staleness = use_staleness
+        self.staleness_lambda = staleness_lambda
 
     def aggregate(
         self,
@@ -47,11 +47,14 @@ class Aggregator:
         update_weights = normalize_weights(update_weights).to(device)
         update_delays = torch.tensor(update_delays).long().to(device)
 
-        if self.use_staleness:
-            # Our simpler staleness weighting
-            staleness_weights = torch.ones(len(updates)).to(device) * cur_epoch + 1e-8
-            staleness_weights -= update_delays
-            update_weights = normalize_weights(staleness_weights).to(device)
+        # Our simpler staleness weighting
+        staleness_weights = torch.ones(len(updates)).to(device) * cur_epoch + 1e-8
+        staleness_weights -= update_delays * self.staleness_lambda
+        # before normalization, the values of staleness_weights must be nonnnegative
+        staleness_weights += staleness_weights.min()
+        staleness_weights = normalize_weights(staleness_weights).to(device)
+
+        update_weights = normalize_weights(update_weights + staleness_weights)
 
         return points, update_weights
 